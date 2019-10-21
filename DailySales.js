1c5724375cbe519fd1d3b9102e73fcb4b36768cc
Songling Lu <38961149+LaRichelieu@users.noreply.github.com>
2019-10-21 07:03:04 -0700

    All Charts

---
+++ DailySales.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    Highcharts.chart('DailySales', {
+        chart: {
+            type: 'line'
+        },
+
+        title: {
+            text: 'Average Sales on Different Day of the Week'
+        },
+
+        xAxis: {
+            categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+            'Friday', 'Saturday', 'Sunday'],
+        },
+
+        yAxis: {
+            title: {
+                text: 'Average Sales'
+            }
+        },
+
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+
+        series: [{
+            name: 'Average Sales',
+            data: [303163.75, 300023.25, 303959.5, 303729.75,
+            310587, 294333.4, 296574.4]
+        }]
+    });
+});
